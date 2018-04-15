3e1ca7566821a1d878f41a775afa0814e237b9c7
Martial Saunois <martial@saunois.net>
2018-04-15 13:22:11 +0200

    Add a counter of attempts

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
+    let mut counter = 0;
 
     loop {
         println!("Please input your guess.");
@@ -23,13 +24,14 @@ fn main() {
             Err(_) => continue,
         };
 
+        counter = counter + 1;
         println!("You guessed: {}", guess);
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
-                println!("You win!");
+                println!("You win in {} attempts!", counter);
                 break;
             }
         }
